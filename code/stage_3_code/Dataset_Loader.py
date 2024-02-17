8fe43d6b03a420d34577e8c9345195a3a4d130b0
Ryan Yu <98731415+ryyu444@users.noreply.github.com>
2024-02-17 01:11:23 -0800

    Update Dataset_Loader.py

--- code/stage_3_code/Dataset_Loader.py
+++ code/stage_3_code/Dataset_Loader.py
@@ -3,6 +3,8 @@ import pickle
 import numpy as np
 import torch
 from torchvision.transforms import functional as F
+import torchvision.transforms as transforms
+from PIL import Image
 
 class Dataset_Loader(dataset):
     data = None
@@ -18,6 +20,12 @@ class Dataset_Loader(dataset):
         X_test = []
         y_test = []
 
+        transform = transforms.Compose([
+          # transforms.Resize((64, 64)),  # Resize to 64x64
+          transforms.ToTensor(),  # Convert to PyTorch Tensor
+          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize        
+        ])
+
         is_orl_dataset = False
 
         f = open(self.dataset_source_folder_path + file_name, 'rb')
@@ -26,32 +34,34 @@ class Dataset_Loader(dataset):
 
         if file_name == "ORL":
             is_orl_dataset = True
-
-        # 3, 112, 92
+        
         for instance in data['train']:
             image_matrix = instance['image']
-            # print(image_matrix.shape)
 
-            # Append grayscale image to X_train
             if is_orl_dataset:
                 image_matrix = torch.Tensor(image_matrix).view(3, 112, 92)
                 image_matrix = F.rgb_to_grayscale(image_matrix)
 
-            # print(image_matrix.shape)
+            pil_image = Image.fromarray(np.uint8(image_matrix)).convert('RGB')
+            image_matrix = transform(pil_image).numpy()
             X_train.append(image_matrix)
             y_train.append(instance['label'])
+
+        print("Finished loading and transforming train data")  
+
         for instance in data['test']:
-            image_matrix = instance['image']
+          image_matrix = instance['image']
+          # image_matrix = image_matrix / 255.0 # normalization of pixel values
+          if is_orl_dataset:
+              image_matrix = torch.Tensor(image_matrix).view(3, 112, 92)
+              image_matrix = F.rgb_to_grayscale(image_matrix)
+          
+          pil_image = Image.fromarray(np.uint8(image_matrix)).convert('RGB')
+          image_matrix = transform(pil_image).numpy()
+          X_test.append(image_matrix)
+          y_test.append(instance['label'])
 
-            # Append grayscale image to X_test
-            if is_orl_dataset:
-                image_matrix = torch.Tensor(image_matrix).view(3, 112, 92)
-                image_matrix = F.rgb_to_grayscale(image_matrix)
-
-            X_test.append(image_matrix)
-            y_test.append(instance['label'])
-
-        print("Number of unique in y test", np.unique(y_train))
+        print("Finished loading and transforming train data") 
 
         return X_train, y_train, X_test, y_test
 
@@ -59,4 +69,6 @@ class Dataset_Loader(dataset):
         print('loading data...')
 
         train_features, train_labels, test_features, test_labels = self.parse(self.dataset_file_name)
+
         return {'X_train': train_features, 'y_train': train_labels, 'X_test': test_features, 'y_test': test_labels}
+        
