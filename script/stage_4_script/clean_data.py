b978dce79dfa786df40a492429adadb4fc44d34a
Brian Li <brianli4824@gmail.com>
2024-02-24 13:50:34 -0800

    make neg sentiment 0

--- script/stage_4_script/clean_data.py
+++ script/stage_4_script/clean_data.py
@@ -37,9 +37,9 @@ def create_csv(directory):
             
             # append new row to data frame
             if sentiment == "pos":
-                new_row = {'Tokens': stemmed, 'Sentiment': 1}
+                new_row = {'Tokens': stemmed, 'Sentiment': 1}   # positive sentiment denoted as 1
             else:
-                new_row = {'Tokens': stemmed, 'Sentiment': -1}
+                new_row = {'Tokens': stemmed, 'Sentiment': 0}   # negative sentiment denoted as 0
 
             df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 
