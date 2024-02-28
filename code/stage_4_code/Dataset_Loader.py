4350d161be8efb9a1141a7821e9ce271f1bdfd7f
adityaseth8 <aseth@ucdavis.edu>
2024-02-28 11:46:18 -0800

    added barebones structure for generation. Works up until train. Need to adjust from CEL to MSE

--- code/stage_4_code/Dataset_Loader.py
+++ code/stage_4_code/Dataset_Loader.py
@@ -6,21 +6,38 @@ Concrete IO class for a specific dataset
 # License: TBD
 
 from code.base_class.dataset import dataset
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import ast
-
+import torchtext
+import os
+glove = torchtext.vocab.GloVe(name="6B", dim=300)
 
 class Dataset_Loader(dataset):
     data = None
     dataset_source_folder_path = None
     dataset_train_file_name = None
     dataset_test_file_name = None
+    embed_dim = 300     # must be the same as the glove dim
+    GLOVE_FILE = os.path.join(".vector_cache", f"glove.6B.{embed_dim}d.txt")
+    average_word_embed = []
     
     def __init__(self, dName=None, dDescription=None):
         super().__init__(dName, dDescription)
+        with open("./data/stage_4_data/text_classification/average_word_embed.txt", "r") as f:
+            lines = f.readlines()
+            f.close()
+            for line in lines:
+                self.average_word_embed.append(np.float32(line))
 
-    def clean_string(string):
+        # Create a new word in the vocab with the average word embedding
+        glove.stoi["unk_token"] = len(glove.stoi)
+        glove.itos.append("unk_token")
+        glove.vectors = np.vstack([glove.vectors, self.average_word_embed])  # Append the new embedding to the existing embeddings
+
+
+    def clean_string(self, string):
         cleanStr = ''
 
         for char in string:
@@ -28,7 +45,7 @@ class Dataset_Loader(dataset):
                 cleanStr += char
 
         return cleanStr
-
+        
     def parse(self, file_name):
         X = []  # tokens
         y = []  # sentiment
@@ -36,25 +53,19 @@ class Dataset_Loader(dataset):
         df = df.sample(frac = 1) # shuffle the data
 
         for i, row in df.iterrows():
-            # row = list(row[])
             a_list = ast.literal_eval(row["Tokens"])
-            # print(len(a_list))
             X.append(a_list)
-            # print(X)
-            # print("`````````````````````````")
-            # print(X[0][0])
-            # exit(0)
             y.append(row["Sentiment"])
         
         # X = np.array(X)
         y = np.array(y)
         return X, y
     
-    def parse_jokes(self, file_name): ## NOT DONE YET
+    def parse_jokes(self, file_name):     
         X, y = [], []
-        sliding_window = 5
-        next(f) # ignore line 1        
+        sliding_window = 5       
         f = open(self.dataset_source_folder_path + file_name, 'r')
+        next(f) # ignore line 1 
         for line in f:
             line = line.strip("\n")
             joke = line.split(",", 1)[1]
@@ -63,32 +74,45 @@ class Dataset_Loader(dataset):
 
             # remove punctuation
             tokens = [self.clean_string(t).lower() for t in tokens]
-            print(tokens)
+            # print(tokens)
             
+            # convert to glove numerical indices here..
+            seq_indices = []
+            for word in tokens:
+                if word in glove.stoi:                      # Word is in vocab
+                    seq_indices.append(glove.stoi[word])
+                else: # Out of vocab words are excluded     # If word is not in vocab, append unknown token
+                    seq_indices.append(glove.stoi["unk_token"])
+            # print("Seq indices", seq_indices)
             # sliding window
             # tokens 10, s_w 5 --> [0,4] + [5], [1,5] + [6], [2,6] + [7], [3,7] + [8], [4,8] + [9]
             # 10 - 5 = 5 -> range(5) = 0 1 2 3 4.
-            for i in range(0, len(tokens) - sliding_window):
-                input_sequence = tokens[i:i + sliding_window]
-                correct_next_token = [tokens[i + sliding_window]]
-
+            for i in range(0, len(seq_indices) - sliding_window):
+                input_sequence = seq_indices[i:i + sliding_window]
+                correct_next_token = [seq_indices[i + sliding_window]]
+                # print("~~~~")
+                # print(input_sequence, " FOLLOWED BY ", correct_next_token)
                 X.append(input_sequence)
                 y.append(correct_next_token)
-
+            # print("X", X, "followed by", y)
         f.close()
-
+        # print(len(X))
+        # print(len(y))
         return X, y
-        
-
-
 
     def load(self):
         print('loading data...')
-        train_features, train_labels = self.parse(self.dataset_train_file_name)
-        test_features, test_labels = self.parse(self.dataset_test_file_name)
+        train_features, train_labels, test_features, test_labels = None, None, None, None
+        if self.dataset_train_file_name == "jokes_data":
+            X, y = self.parse_jokes(self.dataset_train_file_name)
+            train_features, test_features, train_labels, test_labels = train_test_split(X, y, test_size=0.2)
+        else:
+            train_features, train_labels = self.parse(self.dataset_train_file_name)
+            test_features, test_labels = self.parse(self.dataset_test_file_name)
+
         return {'X_train': train_features, 'y_train': train_labels, 'X_test': test_features, 'y_test': test_labels}
 
-#           Data Format
+#           IMDB Data Format
 # X = [
 #       [   ["tokens per review"]  , int ],
 #       [   ["tokens per review"]  , int ],
---
+++ script/stage_4_script/script_text_generation.py
@@ -0,0 +1,43 @@
+from code.stage_4_code.Dataset_Loader import Dataset_Loader
+from code.stage_4_code.Method_Generation import Method_Generation
+from code.stage_4_code.Result_Saver import Result_Saver
+from code.stage_4_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_4_code.Setting import Setting
+import numpy as np
+import torch
+
+
+
+# ---- Multi-Layer Perceptron script ----
+if 1:
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    # ------------------------------------------------------
+
+    # ---- objection initialization section ---------------
+    data_obj = Dataset_Loader('train', '')
+    data_obj.dataset_source_folder_path = './data/stage_4_data/'
+    data_obj.dataset_train_file_name = 'jokes_data'
+
+    method_obj = Method_Generation('RNN text generation', '')
+
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = './result/stage_4_result/RNN_'
+    result_obj.result_destination_file_name = 'generation_result'
+
+    setting_obj = Setting('train test split', '')
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print('************ Start ************')
+
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+    mean_score, std_score = setting_obj.load_run_save_evaluate()
+    print('************ Overall Performance ************')
+    print('Text Generation Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
+    print('************ Finish ************')
+    # ------------------------------------------------------
