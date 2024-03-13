0c9deb8b72f67ff760bcf265ae8cbec8543b6db0
Ryan <ryyu444@gmail.com>
2024-03-12 19:36:44 -0700

    cleaned up Setting

--- code/stage_5_code/Setting.py
+++ code/stage_5_code/Setting.py
@@ -5,9 +5,6 @@ import torch
 class Setting(setting):
     def load_run_save_evaluate(self):
         data = self.dataset.load()
-        # X_train, y_train, X_test, y_test = data['X_train'], data['y_train'], data['X_test'], data['y_test']
-        # adj_train = data['graph']['utility']['A_train']
-        # adj_test = data['graph']['utility']['A_test']
         
         X, y = data['graph']['X'], data['graph']['y']
         adj = data['graph']['utility']['A']
@@ -15,18 +12,6 @@ class Setting(setting):
         test_idx = data['test_idx']
         
         self.method.data = {'X': X, 'y': y, 'adj': adj, 'train_idx': train_idx, 'test_idx': test_idx}
-        
-        # adj_train = data['graph']['utility']['A']
-        # adj_test = data['graph']['utility']['A']    # TO FIX
-        
-        
-        
-        # run MethodModule
-        # self.method.data = {'train': {'X': X, 'y': y, 'adj': adj_train}, 
-        #                     'test': {'X': X_test, 'y': y_test, 'adj': adj_test},
-        #                     'train_idx': data['train_idx'],
-        #                     'test_idx': data['test_idx']
-        #                     }
         learned_data = self.method.run()
 
         # save raw dataModule
